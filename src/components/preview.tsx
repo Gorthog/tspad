410c95025ba362aa33deae1fc7b49b422c57825b
Amir Shiri <amir@sinapistech.com>
2022-06-08 13:56:23 +0300

    add error handling for async code

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -12,15 +12,26 @@ const html = `
       <body>
         <div id="root">
         <script>
+          const handleError = (e) => {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime error</h4>' + e + '</div>';
+            console.error(e);
+          }
+
+          window.addEventListener('error', (event) => {
+            event.preventDefault();
+            handleError(event.error);
+          });
+
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
             } catch (e) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime error</h4>' + e + '</div>';
-              console.error(e);
+              handleError(e);
             }
           }, false)
+
+
 
         </script>
       </body>
