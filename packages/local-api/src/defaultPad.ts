af5bc280a20689ac796ce3c01f6b217d1d044fa0
Amir Shiri <amir@sinapistech.com>
2022-06-23 13:01:18 +0300

    update defaults

--- packages/local-api/src/defaultPad.ts
+++ packages/local-api/src/defaultPad.ts
@@ -3,13 +3,13 @@ const defaultPad = [
     id: "khfEFOIhD86RNmp358APT",
     type: "text",
     content:
-      "# tspad\n\nThis is an interactive coding environment. You can write Typescript, see it executed, and write comprehensive documentation using markdown.\n\n- Click any text cell (**including this one**) to edit it\n- If you export variables from one cell, you can import it via another. The editor will even give you a suggestion for quick fix.\n- Editor provides autocomplete, type hints, and red squiggles on errors.\n- You can show any React component, string, number, JSON object or anything else by calling the `show` function. This is a function built into this environment. Call show multiple times to show multiple values\n- Re-order or delete cells using the buttons on the top right\n- Add new cells by hovering on the divider between each cell\n \nAll of your changes get saved to the file you opened JBook with. So if you ran `npx jbook serve test.js`, all of the text and code you write will be saved to the `test.js` file.",
+      "# tspad\n\nThis is an interactive coding environment. You can write Typescript, see it executed, and write comprehensive documentation using markdown.\n\n- Click any text cell (**including this one**) to edit it\n- You can import it via another. The editor will even give you a suggestion for quick fix.\n- Editor provides autocomplete, type hints, and red squiggles on errors.\n- You can show any React component, string, number, JSON object or anything else by calling the `show` function. This is a function built into this environment. Call show multiple times to show multiple values\n- Re-order or delete cells using the buttons on the top right\n- Add new cells by hovering on the divider between each cell\n \nAll of your changes get saved to the file you opened JBook with. So if you ran `npx jbook serve test.js`, all of the text and code you write will be saved to the `test.js` file.",
   },
   {
     id: "PC9cjHfr955IFTxVurs4h",
     type: "code",
     content:
-      "import { useState } from 'react';\r\nconst a = 6;\r\nconst Counter = () => {\r\n  const [count, setCount] = useState<number>(0);\r\n  return (\r\n    <div>\r\n      <button onClick={() => setCount(count + 1)}>Click</button>\r\n      <h3>Count: {count}</h3>\r\n    </div>\r\n  );\r\n};\r\n\r\nshow(<Counter />)",
+      "import { useState } from 'react';\r\nconst Counter = () => {\r\n  const [count, setCount] = useState<number>(0);\r\n  return (\r\n    <div>\r\n      <button onClick={() => setCount(count + 1)}>Click</button>\r\n      <h3>Count: {count}</h3>\r\n    </div>\r\n  );\r\n};\r\n\r\nshow(<Counter />)",
   },
   {
     id: "EcA7V7WeVuNGho4tScE2_",
